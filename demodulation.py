bb404e13baad28738aec2afb4acbd2ac5226ddbb
nepsydude <prabesh91@hotmail.com>
2020-06-11 00:28:00 +0200

    Adding noise to the signal

--- demodulation.py
+++ demodulation.py
@@ -16,12 +16,15 @@ def qpsk_demodule(r):
     
     x=r*np.cos(2*np.pi*fc*t) # I arm
     y=-r*np.sin(2*np.pi*fc*t) # Q arm
+    """discrete, linear convolution of two one-dimensional sequences
+        with numpy.convolve"""
     x = np.convolve(x,np.ones(L)) # integrate for L (Tsym=2*Tb) duration
     y = np.convolve(y,np.ones(L)) #integrate for L (Tsym=2*Tb) duration
     
     x = x[L-1::L] # I arm - sample at every symbol instant Tsym
     y = y[L-1::L] # Q arm - sample at every symbol instant Tsym
-    bits_det = np.zeros(2*len(x))
+    
+    bits_det = np.zeros(2*len(x)) # output bits after detection
     bits_det[0::2] = (x>0) # even bits
     bits_det[1::2] = (y>0) # odd bits
     
--- simulation.py
+++ simulation.py
@@ -18,22 +18,21 @@ EbN0dB = np.arange(-4,11,2) # Eb/N0 range in dB
 fc = 100 # carrier frequency in Hz
 OF = 8 # over sampling factor
 fs = OF*fc #sampling frequency
-L = 2*OF #samples in each symbol
-#snrdB = 30  
-BER = np.zeros(len(EbN0dB)) 
+#L = 2*OF #samples in each symbol
+BER = np.zeros(len(EbN0dB))  # BER values for each Eb/N0
 
 bits = np.random.randint(2, size=N) # random 0's and 1's of Size N
 
-result = qpsk_mod(bits,fc,fs)
-s = result['s(t)']
+result = qpsk_mod(bits,fc,fs) # modulated qpsk components
+s = result['s(t)']  # values of signal in a dictionary
 
 
 
 
 for i, EbN0 in enumerate(EbN0dB):
-    
+    #Adding noise
     r = AWGN(s,N)
-    bits_det = qpsk_demodule(r)
-    BER[i] = np.sum(bits!=bits_det)/N
+    bits_det = qpsk_demodule(r) # QPSK demodulation
+    BER[i] = np.sum(bits!=bits_det)/N # computation of BER
     
-theoreticalBER = 0.5*erfc(np.sqrt(10**(EbN0dB/10)))
+theoreticalBER = 0.5*erfc(np.sqrt(10**(EbN0dB/10)))  # theoretical value of BER
